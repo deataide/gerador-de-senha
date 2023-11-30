fba945444c585c520f7da6bcd61736c16b64ad76
deataide <deividdimasp4@hotmail.com>
2023-11-30 20:39:33 -0300

    formatado

--- js/skeletons.js
+++ js/skeletons.js
@@ -1,21 +1,21 @@
-const passwordInput = document.querySelector('#passwordId');
-const passwordLength = document.querySelector('#lengthId');
+const passwordInput = document.querySelector("#passwordId");
+const passwordLength = document.querySelector("#lengthId");
 const passwordLengthLabel = document.querySelector('label[for="lengthId"]');
-const specialText = document.querySelector('#specialTextId');
-const generateButton = document.querySelector('#btnGerar');
+const specialText = document.querySelector("#specialTextId");
+const generateButton = document.querySelector("#btnGerar");
+const generateButton2 = document.querySelector("#btnGerar1");
+const copy = document.querySelector("#copyId");
 
-const a = document.querySelector('#aheader')
+const a = document.querySelector("#aheader");
 
-const copy = document.querySelector('#copyId');
-
-const chkAll = document.querySelector('#chkAll');
-const chkUpper = document.querySelector('#chkUppercase');
-const chkLower = document.querySelector('#chkLowercase');
-const chkSpecial = document.querySelector('#chkSpecial');
-const chkNumbers = document.querySelector('#chkNumbers');
+const chkAll = document.querySelector("#chkAll");
+const chkUpper = document.querySelector("#chkUppercase");
+const chkLower = document.querySelector("#chkLowercase");
+const chkSpecial = document.querySelector("#chkSpecial");
+const chkNumbers = document.querySelector("#chkNumbers");
 
 const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const symbols = ['!', '@', '#', '$', '%', '8', '=', '@', '!', '#'];
+const symbols = ["!", "@", "#", "$", "%", "8", "=", "@", "!", "#"];
 const caracters = Array.from(Array(26)).map((_, i) => i + 97);
 const lowercaseCaracters = caracters.map((item) => String.fromCharCode(item));
 const uppercaseCaracters = lowercaseCaracters.map((item) => item.toUpperCase());
@@ -28,11 +28,21 @@ allCaracters.push(...symbols);
 allCaracters.push(...lowercaseCaracters);
 allCaracters.push(...uppercaseCaracters);
 
-passwordLength.addEventListener('change', () => {
+passwordLength.addEventListener("change", () => {
   passwordLengthLabel.innerHTML = passwordLength.value;
 });
 
-generateButton.addEventListener('click', () => {
+generateButton.addEventListener("click", () => {
+  generatePassword(
+    chkAll.checked,
+    chkUpper.checked,
+    chkLower.checked,
+    chkSpecial.checked,
+    chkNumbers.checked,
+    passwordLength.value
+  );
+});
+generateButton2.addEventListener("click", () => {
   generatePassword(
     chkAll.checked,
     chkUpper.checked,
@@ -43,8 +53,8 @@ generateButton.addEventListener('click', () => {
   );
 });
 
-document.addEventListener('keypress', (e) => {
-  if (e.key == 'Enter') {
+document.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
     generatePassword(
       chkAll.checked,
       chkUpper.checked,
@@ -64,7 +74,7 @@ function generatePassword(
   hasNumbers,
   length
 ) {
-  passwordInput.value = '';
+  passwordInput.value = "";
 
   let newArray = [
     ...(hasAll ? allCaracters : []),
@@ -78,9 +88,9 @@ function generatePassword(
     newArray = allCaracters;
   }
 
-  const specialTrated = specialText.value.replace(/\s/g, '');
+  const specialTrated = specialText.value.replace(/\s/g, "");
 
-  let password = '';
+  let password = "";
 
   if (specialTrated) {
     password += specialTrated;
@@ -100,15 +110,10 @@ function passwordCopy() {
   copyText.select();
   copyText.setSelectionRange(0, 99999);
 
-  document.execCommand('copy');
+  document.execCommand("copy");
   alert(`Text copied: ${copyText.value}`);
 }
 
-copy.addEventListener('click', () => {
+copy.addEventListener("click", () => {
   passwordCopy();
 });
-
-a.addEventListener('click', (e)=>{
-  e.preventDefault()
-  alert('Em breve')
-})
